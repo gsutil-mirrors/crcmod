d9d93cecc1eea78f95975d5ac4bc845857cd4379
Craig McQueen <mcqueen-c@edsrd1.yzk.co.jp>
2010-01-11 14:13:47 +0900

    For Python 2.x and 3.x, add check values to table. This is in preparation for more unit tests of the predefined algorithms.

--- py3/crcmod/predefined.py
+++ py3/crcmod/predefined.py
@@ -36,25 +36,25 @@ import crcmod
 
 __all__ = [ 'PredefinedCrc' ]
 
-REFLECT = True
-NON_REFLECT = False
+REVERSE = True
+NON_REVERSE = False
 
 _crc_definitions_table = [
-#       Name                Identifier-name,    Poly            Reflect         Init-value      XOR-out         
-    [   'crc-8',            'Crc8',             0x107,          NON_REFLECT,    0x00,           0x00,       ],
-    [   'crc-16',           'Crc16',            0x18005,        REFLECT,        0x0000,         0x0000,     ],
-    [   'crc-16-usb',       'Crc16Usb',         0x18005,        REFLECT,        0xFFFF,         0xFFFF,     ],
-    [   'x-25',             'CrcX25',           0x11021,        REFLECT,        0xFFFF,         0xFFFF,     ],
-    [   'kermit',           'CrcKermit',        0x11021,        REFLECT,        0x0000,         0x0000,     ],
-    [   'crc-ccitt-false',  'CrcCcittFalse',    0x11021,        NON_REFLECT,    0xFFFF,         0x0000,     ],
-    [   'crc-ccitt',        'CrcCcitt',         0x11021,        NON_REFLECT,    0x1D0F,         0x0000,     ],
-    [   'crc-32',           'Crc32',            0x104c11db7,    REFLECT,        0xFFFFFFFF,     0xFFFFFFFF, ],
-    [   'crc-32c',          'Crc32C',           0x11edc6f41,    REFLECT,        0xFFFFFFFF,     0xFFFFFFFF, ],
-    [   'crc-32-mpeg',      'Crc32Mpeg',        0x104c11db7,    NON_REFLECT,    0xFFFFFFFF,     0x00000000, ],
+#       Name                Identifier-name,    Poly            Reverse         Init-value      XOR-out     Check
+    [   'crc-8',            'Crc8',             0x107,          NON_REVERSE,    0x00,           0x00,       0xF4,       ],
+    [   'crc-16',           'Crc16',            0x18005,        REVERSE,        0x0000,         0x0000,     0xBB3D,     ],
+    [   'crc-16-usb',       'Crc16Usb',         0x18005,        REVERSE,        0xFFFF,         0xFFFF,     0xB4C8,     ],
+    [   'x-25',             'CrcX25',           0x11021,        REVERSE,        0xFFFF,         0xFFFF,     0x906E,     ],
+    [   'kermit',           'CrcKermit',        0x11021,        REVERSE,        0x0000,         0x0000,     0x2189,     ],
+    [   'crc-ccitt-false',  'CrcCcittFalse',    0x11021,        NON_REVERSE,    0xFFFF,         0x0000,     0x29B1,     ],
+    [   'crc-ccitt',        'CrcCcitt',         0x11021,        NON_REVERSE,    0x1D0F,         0x0000,     0xE5CC,     ],
+    [   'crc-32',           'Crc32',            0x104c11db7,    REVERSE,        0xFFFFFFFF,     0xFFFFFFFF, 0xCBF43926, ],
+    [   'crc-32c',          'Crc32C',           0x11edc6f41,    REVERSE,        0xFFFFFFFF,     0xFFFFFFFF, 0xE3069283, ],
+    [   'crc-32-mpeg',      'Crc32Mpeg',        0x104c11db7,    NON_REVERSE,    0xFFFFFFFF,     0x00000000, 0x0376E6E7, ],
 
 # 64-bit
-#       Name                Identifier-name,    Poly                    Reflect         Init-value          XOR-out             
-    [   'crc-64-jones',     'Crc64Jones',       0x1ad93d23594c935a9,    REFLECT,        0x0000000000000000, 0x0000000000000000, ],
+#       Name                Identifier-name,    Poly                    Reverse         Init-value          XOR-out             Check
+    [   'crc-64-jones',     'Crc64Jones',       0x1ad93d23594c935a9,    REVERSE,        0x0000000000000000, 0x0000000000000000, 0xE9C6D914C4B8D9CA, ],
 ]
 
 
@@ -78,7 +78,7 @@ _crc_definitions_by_name = {}
 _crc_definitions_by_identifier = {}
 _crc_definitions = []
 
-_crc_table_headings = [ 'name', 'identifier', 'poly', 'reflect', 'init', 'xor_out' ]
+_crc_table_headings = [ 'name', 'identifier', 'poly', 'reverse', 'init', 'xor_out' ]
 
 for table_entry in _crc_definitions_table:
     crc_definition = dict(zip(_crc_table_headings, table_entry))
@@ -97,7 +97,7 @@ class PredefinedCrc(crcmod.Crc):
             definition = _crc_definitions_by_identifier.get(crc_name, None)
         if not definition:
             raise KeyError("Unkown CRC name '{0}'".format(crc_name))
-        super().__init__(poly=definition['poly'], initCrc=definition['init'], rev=definition['reflect'], xorOut=definition['xor_out'])
+        super().__init__(poly=definition['poly'], initCrc=definition['init'], rev=definition['reverse'], xorOut=definition['xor_out'])
 
 
 # crcmod.predefined.Crc is an alias for crcmod.predefined.PredefinedCrc
