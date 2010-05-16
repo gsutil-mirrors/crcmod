898e59076a70a945eadcd14e526cc5aa76b0e691
Craig McQueen <craig@mcqueen.id.au>
2010-05-16 10:08:14 +0900

    Separate license into separate LICENSE file. Add PyPI categories to the setup files.

--- setup.py
+++ setup.py
@@ -23,4 +23,26 @@ ext_modules=[
     Extension('crcmod._crcfunext', [ base_dir + '/src/_crcfunext.c', ],
     ),
 ],
+
+long_description=open('README').read(),
+
+license="MIT",
+classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: Information Technology',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: C',
+    'Programming Language :: C++',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Topic :: Utilities',
+],
 )
