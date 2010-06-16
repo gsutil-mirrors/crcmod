6ff3143680105f66e9b8ec7c5776430b0069650f
Craig McQueen <mcqueen-c@edsrd1.yzk.co.jp>
2010-06-16 13:23:48 +0900

    Remove old test code for Python 2.x and 3.x. Update common test function to run the new unittest-based tests. Update documentation.
    --HG--
    branch : unittest

--- test/test_crcmod.py
+++ test/test_crcmod.py
@@ -1,19 +1,6 @@
 
-import imp
-import os
-import sys
+import unittest
 
-if sys.version_info[0] == 2:
-    version_dir = 'py2'
-elif sys.version_info[0] == 3:
-    version_dir = 'py3'
+import crcmod.test
 
-test_file_name = os.path.join(
-    os.path.dirname(__file__),
-    '..',
-    version_dir,
-    'test',
-    'test_crcmod.py'
-)
-
-imp.load_source("test_crcmod", test_file_name)
+unittest.main(module=crcmod.test)
